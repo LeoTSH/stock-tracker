f8ed85deabfe53deed3ea38da59795dea2739cd5
Leo Tay <leotaysh@gmail.com>
2021-01-13 17:34:55 +0800

    Test for dag

--- test.sh
+++ test.sh
@@ -3,5 +3,5 @@
 # SRC="937fae72dc8a:/var/jenkins_home/workspace/Testing pipeline/test_dag.py"
 SRC="/var/jenkins_home/workspace/Testing pipeline/test_dag.py"
 echo ${SRC}
-cp ${SRC} /var/airflow/dags
+cp "${SRC}" /var/airflow/dags
 # docker cp "${SRC}" /var
